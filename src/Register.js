995e002869cc3effe94b4264ebf5d87fd9b10541
Nomddaay <92080450+Nomddaay@users.noreply.github.com>
2023-06-25 15:19:36 +0700

    updated

--- src/Register.js
+++ src/Register.js
@@ -1,11 +1,10 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef, useState, useEffect } from "react";
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import axios from './api/axios';
 
-// Regex for validation
-const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
-const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[A-Z])(?=.*[0-9])?=.*[!@#$%]).{8,24}$/;
+const USER_REGEX = /^[A-z][A-z0-9-_]{3,23}$/;
+const PWD_REGEX = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#$%]).{8,24}$/;
 const REGISTER_URL = '/register';
 
 const Register = () => {
@@ -15,7 +14,7 @@ const Register = () => {
     const [user, setUser] = useState('');
     const [validName, setValidName] = useState(false);
     const [userFocus, setUserFocus] = useState(false);
- 
+
     const [pwd, setPwd] = useState('');
     const [validPwd, setValidPwd] = useState(false);
     const [pwdFocus, setPwdFocus] = useState(false);
@@ -29,35 +28,28 @@ const Register = () => {
 
     useEffect(() => {
         userRef.current.focus();
-    }, []);
+    }, [])
 
     useEffect(() => {
-        const result = USER_REGEX.test(user);
-        console.log(result);
-        console.log(user);
-        setValidName(result);
-    }, [user]);
+        setValidName(USER_REGEX.test(user));
+    }, [user])
 
     useEffect(() => {
-        const result = PWD_REGEX.test(pwd);
-        console.log(result);
-        console.log(pwd);
-        setValidPwd(result);
-        const match = pwd === matchPwd;
-        setValidMatch(match);
-    },[pwd, matchPwd]);
+        setValidPwd(PWD_REGEX.test(pwd));
+        setValidMatch(pwd === matchPwd);
+    }, [pwd, matchPwd])
 
-    useEffect(() =>{
+    useEffect(() => {
         setErrMsg('');
-    },[user, pwd, matchPwd]);
+    }, [user, pwd, matchPwd])
 
-    const handleSubmmit = async (e) => {
+    const handleSubmit = async (e) => {
         e.preventDefault();
-        // if button enabled with Js hack
-        const v1 = USER_REGEX.test(user)
-        const v2 = PWD_REGEX.test(pwd)
+        // if button enabled with JS hack
+        const v1 = USER_REGEX.test(user);
+        const v2 = PWD_REGEX.test(pwd);
         if (!v1 || !v2) {
-            setErrMsg('Invalid Entry');
+            setErrMsg("Invalid Entry");
             return;
         }
         try {
@@ -68,12 +60,11 @@ const Register = () => {
                     withCredentials: true
                 }
             );
-            console.log(response?.data);
-            console.log(response?.accessToken);
-            console.log(JSON.stringify(response))
+            // TODO: remove console.logs before deployment
+            console.log(JSON.stringify(response?.data));
+            //console.log(JSON.stringify(response))
             setSuccess(true);
             //clear state and controlled inputs
-            //need value attrib on inputs for this
             setUser('');
             setPwd('');
             setMatchPwd('');
@@ -89,51 +80,45 @@ const Register = () => {
         }
     }
 
-    return(
+    return (
         <>
-        {success ? (
+            {success ? (
                 <section>
                     <h1>Success!</h1>
                     <p>
-                        <a href='#'>Sign In</a>
+                        <a href="#">Sign In</a>
                     </p>
                 </section>
-            ) : (   
-           
+            ) : (
                 <section>
-                    <p ref={errRef} className={errMsg ? "errmsg" : 
-                    "offscreen"} aria-live="assertive">{errMsg}</p>
+                    <p ref={errRef} className={errMsg ? "errmsg" : "offscreen"} aria-live="assertive">{errMsg}</p>
                     <h1>Register</h1>
-                    <form onSubmit={handleSubmmit}>
+                    <form onSubmit={handleSubmit}>
                         <label htmlFor="username">
                             Username:
-                            <span className={validName ? "valid" : "hide"}>
-                                <FontAwesomeIcon icon={faCheck} />
-                            </span>
-                            <span className={validName || !user ? "hide" : "invalid"}>
-                                <FontAwesomeIcon icon={faTimes} />
-                            </span>
+                            <FontAwesomeIcon icon={faCheck} className={validName ? "valid" : "hide"} />
+                            <FontAwesomeIcon icon={faTimes} className={validName || !user ? "hide" : "invalid"} />
                         </label>
-                        <input 
-                            type='text'
-                            id='username'
+                        <input
+                            type="text"
+                            id="username"
                             ref={userRef}
-                            autoComplete='off'
-                            onChange= {(e) => setUser(e.target.value)}
+                            autoComplete="off"
+                            onChange={(e) => setUser(e.target.value)}
                             value={user}
                             required
-                            aria-invalid={validName ? "false": "true"}
-                            aria-describedby='uidnote'
+                            aria-invalid={validName ? "false" : "true"}
+                            aria-describedby="uidnote"
                             onFocus={() => setUserFocus(true)}
                             onBlur={() => setUserFocus(false)}
                         />
-                        <p id="uidnote" className={userFocus && user && 
-                            !validName ? "instructions" : "offscreen"}>
-                                <FontAwesomeIcon icon={faInfoCircle} />
-                                4 to 24 characters. <br />
-                                Must begin with a letter. <br />
-                                Letters, numbers, underscores, hyphens allowed.
+                        <p id="uidnote" className={userFocus && user && !validName ? "instructions" : "offscreen"}>
+                            <FontAwesomeIcon icon={faInfoCircle} />
+                            4 to 24 characters.<br />
+                            Must begin with a letter.<br />
+                            Letters, numbers, underscores, hyphens allowed.
                         </p>
+
 
                         <label htmlFor="password">
                             Password:
@@ -157,6 +142,7 @@ const Register = () => {
                             Must include uppercase and lowercase letters, a number and a special character.<br />
                             Allowed special characters: <span aria-label="exclamation mark">!</span> <span aria-label="at symbol">@</span> <span aria-label="hashtag">#</span> <span aria-label="dollar sign">$</span> <span aria-label="percent">%</span>
                         </p>
+
 
                         <label htmlFor="confirm_pwd">
                             Confirm Password:
@@ -187,11 +173,11 @@ const Register = () => {
                             {/*put router link here*/}
                             <a href="#">Sign In</a>
                         </span>
-                    </p> 
+                    </p>
                 </section>
             )}
         </>
     )
 }
 
-export default Register;
+export default Register
