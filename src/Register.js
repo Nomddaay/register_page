46a5d4b7feb6d2af22a3095a61400d97c1a2aeaa
Nomddaay <92080450+Nomddaay@users.noreply.github.com>
2023-06-25 15:05:15 +0700

    create login.js & axios

--- src/Register.js
+++ src/Register.js
@@ -1,6 +1,7 @@
 import { useRef, useState, useEffect } from 'react';
 import { faCheck, faTimes, faInfoCircle } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import axios from './api/axios';
 
 // Regex for validation
 const USER_REGEX = /^[a-zA-Z][a-zA-Z0-9-_]{3,23}$/;
@@ -24,7 +25,7 @@ const Register = () => {
     const [matchFocus, setMatchFocus] = useState(false);
 
     const [errMsg, setErrMsg] = useState('');
-    const [success, setSucess] = useState(false);
+    const [success, setSuccess] = useState(false);
 
     useEffect(() => {
         userRef.current.focus();
@@ -50,43 +51,91 @@ const Register = () => {
         setErrMsg('');
     },[user, pwd, matchPwd]);
 
-    return(
-        <section>
-            <p ref={errRef} className={errMsg ? "errmsg" : 
-            "offscreen"} aria-live="assertive">{errMsg}</p>
-            <h1>Register</h1>
-            <form>
-                <label htmlFor="username">
-                    Username:
-                    <span className={validName ? "valid" : "hide"}>
-                        <FontAwesomeIcon icon={faCheck} />
-                    </span>
-                    <span className={validName || !user ? "hide" : "invalid"}>
-                        <FontAwesomeIcon icon={faTimes} />
-                    </span>
-                </label>
-                <input 
-                    type='text'
-                    id='username'
-                    ref={userRef}
-                    autoComplete='off'
-                    onChange= {(e) => setUser(e.target.value)}
-                    value={user}
-                    required
-                    aria-invalid={validName ? "false": "true"}
-                    aria-describedby='uidnote'
-                    onFocus={() => setUserFocus(true)}
-                    onBlur={() => setUserFocus(false)}
-                />
-                <p id="uidnote" className={userFocus && user && 
-                    !validName ? "instructions" : "offscreen"}>
-                        <FontAwesomeIcon icon={faInfoCircle} />
-                        4 to 24 characters. <br />
-                        Must begin with a letter. <br />
-                        Letters, numbers, underscores, hyphens allowed.
-                </p>
+    const handleSubmmit = async (e) => {
+        e.preventDefault();
+        // if button enabled with Js hack
+        const v1 = USER_REGEX.test(user)
+        const v2 = PWD_REGEX.test(pwd)
+        if (!v1 || !v2) {
+            setErrMsg('Invalid Entry');
+            return;
+        }
+        try {
+            const response = await axios.post(REGISTER_URL,
+                JSON.stringify({ user, pwd }),
+                {
+                    headers: { 'Content-Type': 'application/json' },
+                    withCredentials: true
+                }
+            );
+            console.log(response?.data);
+            console.log(response?.accessToken);
+            console.log(JSON.stringify(response))
+            setSuccess(true);
+            //clear state and controlled inputs
+            //need value attrib on inputs for this
+            setUser('');
+            setPwd('');
+            setMatchPwd('');
+        } catch (err) {
+            if (!err?.response) {
+                setErrMsg('No Server Response');
+            } else if (err.response?.status === 409) {
+                setErrMsg('Username Taken');
+            } else {
+                setErrMsg('Registration Failed')
+            }
+            errRef.current.focus();
+        }
+    }
 
-                <label htmlFor="password">
+    return(
+        <>
+        {success ? (
+                <section>
+                    <h1>Success!</h1>
+                    <p>
+                        <a href='#'>Sign In</a>
+                    </p>
+                </section>
+            ) : (   
+           
+                <section>
+                    <p ref={errRef} className={errMsg ? "errmsg" : 
+                    "offscreen"} aria-live="assertive">{errMsg}</p>
+                    <h1>Register</h1>
+                    <form onSubmit={handleSubmmit}>
+                        <label htmlFor="username">
+                            Username:
+                            <span className={validName ? "valid" : "hide"}>
+                                <FontAwesomeIcon icon={faCheck} />
+                            </span>
+                            <span className={validName || !user ? "hide" : "invalid"}>
+                                <FontAwesomeIcon icon={faTimes} />
+                            </span>
+                        </label>
+                        <input 
+                            type='text'
+                            id='username'
+                            ref={userRef}
+                            autoComplete='off'
+                            onChange= {(e) => setUser(e.target.value)}
+                            value={user}
+                            required
+                            aria-invalid={validName ? "false": "true"}
+                            aria-describedby='uidnote'
+                            onFocus={() => setUserFocus(true)}
+                            onBlur={() => setUserFocus(false)}
+                        />
+                        <p id="uidnote" className={userFocus && user && 
+                            !validName ? "instructions" : "offscreen"}>
+                                <FontAwesomeIcon icon={faInfoCircle} />
+                                4 to 24 characters. <br />
+                                Must begin with a letter. <br />
+                                Letters, numbers, underscores, hyphens allowed.
+                        </p>
+
+                        <label htmlFor="password">
                             Password:
                             <FontAwesomeIcon icon={faCheck} className={validPwd ? "valid" : "hide"} />
                             <FontAwesomeIcon icon={faTimes} className={validPwd || !pwd ? "hide" : "invalid"} />
@@ -131,15 +180,17 @@ const Register = () => {
                         </p>
 
                         <button disabled={!validName || !validPwd || !validMatch ? true : false}>Sign Up</button>
-            </form>
-            <p>
+                    </form>
+                    <p>
                         Already registered?<br />
                         <span className="line">
                             {/*put router link here*/}
                             <a href="#">Sign In</a>
                         </span>
-                    </p>
-        </section>
+                    </p> 
+                </section>
+            )}
+        </>
     )
 }
 
